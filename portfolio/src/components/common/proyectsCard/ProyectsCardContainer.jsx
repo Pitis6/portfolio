9b9e596cf9068940d4596677133f4765555f08d9
pitis6 <felipecalvache6@gmail.com>
2023-07-17 23:21:41 -0500

    Creating random function

--- portfolio/src/components/common/proyectsCard/ProyectsCardContainer.jsx
+++ portfolio/src/components/common/proyectsCard/ProyectsCardContainer.jsx
@@ -1,8 +1,8 @@
 import { data } from "../../../utils/data"
 import { ProyectsCard } from "./ProyectsCard"
 
-
-
+const randomIndex = Math.floor(Math.random()*3)
+console.log(randomIndex)
 export const ProyectsCardContainer = () => {
   return (
     data.map((item,index) => ( 
